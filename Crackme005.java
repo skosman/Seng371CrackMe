66516f51fe3b95ef33ead794006f3bdbad5bb3ab
Samantha Kosman <skosman@uvic.ca>
2020-03-11 23:58:57 -0700

    Added final functions to Crackme005.java and created new folder to contain the decompile classes. The new folder contains the results from a Java specific reverse engineering tool, ByteCode Reviewer. Decompiled code is clearer than ghidra and could be good to use as a reference point.

--- Crackme005.java
+++ Crackme005.java
@@ -93,4 +93,63 @@ public class Crackme005 {
     return doubleProcessor - doubleMemory /(double)(long)numProcessors;
 
   }
+
+  // Wont work with ghidra, used ByteCode Viewer
+  // converts a double to a string
+  private static String doubleValue(double num) {
+    return Double.toString(num);
+  }
+
+  // Reads the input from the command line and returns the first line
+  private static String equals() {
+    scan = new Scanner(System.in);
+    return scan.nextLine();
+  }
+
+  // Prints starting strings in the terminal, with the num refering to the crackme number (5)
+  private static void exit(int num) {
+    System.out.println("");
+
+    String str = I.I(364);
+    StringBuilder builtStr = new StringBuilder();
+    builtStr.append(str);
+    builtStr.append(num);
+    System.out.println(builtStr.toString());  // "AGZ Crackme no 5"
+
+    str = I.I(380);
+    System.out.println(str);                  // "There will be some fun in here"
+
+    str = I.I(411);
+    System.out.println(str);                  // "See if you can beat the encryption"
+
+    System.out.println("");
+    return;
+  }
+
+  // Wont work with ghidra, used ByteCode Viewer
+  private static boolean getDecoder(double num1, double num2) {
+    String str1 = doubleValue(num1);
+    String str2 = doubleValue(num2);
+    boolean boo = false;
+    boo = str1.equals(str2);
+    return boo;
+  }
+
+
+  private static String getRuntime(String str) {
+    Base64 decoder = Base64.getDecoder();
+    return String(decoder.decode(str));
+  }
+
+  // Last thing ran in the main function
+  // this corresponds to the endscreen/final 6 lines printed when runnting the jar
+  private static void in() {
+    System.out.println("");
+    System.out.println(I.I(65));     // "Got the solution?"
+    System.out.println(I.I(53));     // "ship it off to me then.."
+    System.out.println(I.I(108));    // "crackme@agz.name"
+    System.out.println(I.I(125));    // "https://www.agz.name"
+    System.out.println(I.I(146));    // "https://www.facebook.com/AGZ.name/"
+
+  }
 }
